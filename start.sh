b40858507d3292efc5a9d7b82a8a92a9f13a6aa5
Gabriele Baldoni <gabriele@x360>
2017-07-03 15:36:48 +0200

    add starting script

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# 
+# File:   start.sh.sh
+# Author: gabriele
+#
+# Created on 3-lug-2017, 15.13.39
+#
+
+#"${OUTPUT_PATH}"  -c /home/gabriele/Video -v -d -i eth0 -a 172.16.0.8
+
+mount Recorded
+echo "Personal is at $1"
+./src/ushare -c ./Recorded -v -d -i eth0 -a $1
+umount Recorded
