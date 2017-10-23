89e525abaf9e96ee03812e26f69a1777c36bbe4b
gabrik <gabriele.baldoni@gmail.com>
2017-10-23 21:17:29 +0200

    fixed conf file

---
+++ conf.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  conf.sh
+#  uShare
+#
+#  Created by Gabriele Baldoni on 23/03/17.
+#  Copyright © 2017 Gabriele Baldoni. All rights reserved.
+#
+./configure --enable-dlna --enable-debug --disable-strip --disable-optimize
